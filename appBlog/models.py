c60fac73a9a24e4dd7375dd95b95892193a85870
Celso Silva <cjrti95@gmail.com>
2021-05-04 21:37:17 -0300

    appBlog funcional

--- appBlog/models.py
+++ appBlog/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+#============================================================================================= GERENCIADOR PERSONALIZADO
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset()\
+                        .filter(status='published')
+
+#=======================================================================================================================
 class Post(models.Model):
-    STATUS_CHOICES=(
+    STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
@@ -26,3 +35,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager()  #gerenciador default
+    published = PublishedManager()  #gerenciador personalizado
+
+#========================================================================================================== URL CANÔNICO
+    def get_abolute_url(self):
+        return reverse('appBlog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
